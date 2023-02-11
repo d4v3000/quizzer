273c005e81383f63eb7ea5706533315d2b98e3da
David Glogaza <david.glogaza40@googlemail.com>
2023-02-11 22:15:53 +0100

    add custom loading

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 function NavBar() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const createQuiz = trpc.quiz.createQuiz.useMutation();
   const router = useRouter();
 
@@ -18,43 +18,58 @@ function NavBar() {
     <nav className="sticky top-0 w-full bg-zinc-900">
       <div className="mx-auto flex w-3/5 justify-between font-semibold text-zinc-400">
         <p className="flex items-center p-3">Logo</p>
-
-        {!session && (
-          <div className="flex gap-4">
-            <Link href="/login">
-              <span className="my-3 ml-4 flex items-center p-1 text-indigo-400 hover:text-white">
-                Log in
-              </span>
-            </Link>
-            <Link href="/signup">
-              <span className="my-3 mr-4 flex items-center rounded-md bg-gradient-to-r from-indigo-600 to-violet-700 px-3 py-1.5 text-sm text-white">
-                Sign up
-              </span>
-            </Link>
-          </div>
-        )}
-        {session && (
-          <div className="flex gap-4">
-            <Link href="/quiz">
-              <span className="my-3 ml-4 flex items-center p-1 text-indigo-400 hover:text-white">
-                Quiz
-              </span>
-            </Link>
-            <button
-              onClick={() => createQuiz.mutate()}
-              disabled={createQuiz.status === "loading"}
-            >
-              {createQuiz.status === "loading" ? (
-                <LoadingSpinner />
-              ) : (
-                "Create Quiz"
+        <div className="flex gap-4">
+          {status === "loading" ? (
+            <div className="flex animate-pulse gap-3">
+              <div className="my-3 ml-4 w-16 rounded-md bg-zinc-600 p-1"></div>
+              <div className="my-3 w-16 rounded-md bg-zinc-600 p-1"></div>
+              <div className="my-3 mr-4 w-16 rounded-md bg-zinc-600 p-1"></div>
+            </div>
+          ) : (
+            <>
+              {!session && (
+                <>
+                  <Link href="/login">
+                    <span className="my-3 ml-4 flex items-center p-1 text-indigo-400 hover:text-white">
+                      Log in
+                    </span>
+                  </Link>
+                  <Link href="/signup">
+                    <span className="my-3 mr-4 flex items-center rounded-md bg-gradient-to-r from-indigo-600 to-violet-700 px-3 py-1.5 text-sm text-white">
+                      Sign up
+                    </span>
+                  </Link>
+                </>
               )}
-            </button>
-            <Button onClick={() => signOut()} intent="primary" size="small">
-              Sign out
-            </Button>
-          </div>
-        )}
+              {session && (
+                <>
+                  <Link href="/quiz">
+                    <span className="my-3 ml-4 flex items-center p-1 text-indigo-400 hover:text-white">
+                      Quiz
+                    </span>
+                  </Link>
+                  <button
+                    onClick={() => createQuiz.mutate()}
+                    disabled={createQuiz.status === "loading"}
+                  >
+                    {createQuiz.status === "loading" ? (
+                      <LoadingSpinner />
+                    ) : (
+                      "Create Quiz"
+                    )}
+                  </button>
+                  <Button
+                    onClick={() => signOut()}
+                    intent="primary"
+                    size="small"
+                  >
+                    Sign out
+                  </Button>
+                </>
+              )}
+            </>
+          )}
+        </div>
       </div>
       <hr className="border-zinc-700" />
     </nav>

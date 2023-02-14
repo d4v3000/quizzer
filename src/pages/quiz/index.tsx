f1bf3fcc7b0bb9f6d551563818d8fb0d7c7f7e23
David Glogaza <david.glogaza40@googlemail.com>
2023-02-14 17:12:03 +0100

    show loading animation while fetching

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -8,7 +8,6 @@ import { trpc } from "../../utils/trpc";
 function Quiz() {
   const getQuizzes = trpc.quiz.getUserQuizzes.useQuery();
   const loadingCards = [...Array(9).keys()];
-
   useEffect(() => {
     getQuizzes.refetch();
   }, []);
@@ -21,7 +20,7 @@ function Quiz() {
         <div
           className={`grid gap-6 text-gray-200 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3`}
         >
-          {getQuizzes.isLoading
+          {getQuizzes.isLoading || getQuizzes.isFetching
             ? loadingCards.map((card) => <LoadingCard key={card} />)
             : getQuizzes.data?.map((quiz) => <QuizCard quiz={quiz} />)}
         </div>

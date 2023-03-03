5805ea372abff21c95f7eb40729a680d3c13dcb5
David Glogaza <david.glogaza40@googlemail.com>
2023-03-03 18:44:04 +0100

    remove email login & add logo + name

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,38 +1,39 @@
 import React from "react";
 import { GoogleOutlined } from "@ant-design/icons";
 import { getProviders, signIn } from "next-auth/react";
+import Image from "next/image";
 
 function Login({ providers }: any) {
-	return (
-		<div className="flex h-screen w-screen items-center bg-black text-zinc-300 justify-center bg-gradient-to-b from-zinc-800 to-zinc-900 pb-40 font-semibold">
-			<div className="flex flex-col gap-4">
-				<div className="flex flex-col items-center gap-3 pb-4">
-					<div>Logo</div>
-					<p className="text-xl">Log in to App</p>
-				</div>
-				<div
-					className="flex cursor-pointer items-center justify-center gap-2 rounded-md border border-indigo-400 bg-indigo-500 py-2 px-20 text-white hover:bg-indigo-400"
-					onClick={() => signIn(providers.google.id, { callbackUrl: "/" })}
-				>
-					<GoogleOutlined className="text-white" />
-					Continue with Google
-				</div>
-				<div
-					className="flex cursor-pointer items-center justify-center rounded-md border border-zinc-700 bg-zinc-800 py-2 hover:bg-zinc-700"
-					onClick={() => signIn(providers.email.id)}
-				>
-					Continue with Email
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black bg-gradient-to-b from-zinc-800 to-zinc-900 pb-40 font-semibold text-zinc-300">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col items-center gap-3 pb-4">
+          <Image
+            src="/quizzerLogo.svg"
+            alt="Quizzer Logo"
+            className="p-2"
+            height={100}
+            width={100}
+          />
+          <p className="text-xl">Log in to Quizzer</p>
+        </div>
+        <div
+          className="flex cursor-pointer items-center justify-center gap-2 rounded-md border border-indigo-400 bg-indigo-500 py-2 px-20 text-white hover:bg-indigo-400"
+          onClick={() => signIn(providers.google.id, { callbackUrl: "/" })}
+        >
+          <GoogleOutlined className="text-white" />
+          Continue with Google
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
 
 export async function getServerSideProps() {
-	const providers = await getProviders();
-	return {
-		props: { providers },
-	};
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
 }

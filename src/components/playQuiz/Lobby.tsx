b797a96136765bd5cf5311b724656fc4d2ed3657
David Glogaza <david.glogaza40@googlemail.com>
2023-04-07 19:41:19 +0200

    add chat ui

--- src/components/playQuiz/Lobby.tsx
+++ src/components/playQuiz/Lobby.tsx
@@ -1,13 +1,16 @@
 import {
   EyeIcon,
   EyeSlashIcon,
+  PaperAirplaneIcon,
   PaperClipIcon,
 } from "@heroicons/react/24/outline";
 import Background from "@ui/Background";
 import Button from "@ui/Button";
+import Input from "@ui/Input";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import * as Tabs from "@radix-ui/react-tabs";
 
 const Lobby = () => {
   const baseUrl = "http://localhost:3000" || process.env.BASE_URL;
@@ -15,11 +18,24 @@ const Lobby = () => {
   const [showInviteLink, setShowInviteLink] = useState(false);
 
   const colors = ["#991b1b", "#1e40af", "#166534", "#6b21a8", "#854d0e"];
+  const messages = [
+    { message: "Player 1 joined the game", sender: "system" },
+    { message: "Hey", sender: "Player 1" },
+    { message: "Player 1 left the game", sender: "system" },
+    { message: "Player 2 joined the game", sender: "system" },
+    { message: "What's up?", sender: "Player 2" },
+  ];
+  const teamMessages = [
+    { message: "Player 1 joined your team", sender: "system" },
+    { message: "Hey", sender: "Player 1" },
+    { message: "Player 2 joined your team", sender: "system" },
+    { message: "What's up?", sender: "Player 2" },
+  ];
   const players = ["Name1", "Name2", "Name3"];
 
   return (
-    <div className="mx-auto grid h-screen w-10/12 grid-flow-col grid-rows-3 gap-6 py-8">
-      <Background className="row-span-2">
+    <div className="mx-auto grid h-screen w-10/12 grid-flow-col grid-cols-5 grid-rows-3 gap-4 py-8">
+      <Background className="col-span-1 row-span-2">
         <div className="flex flex-col gap-8 p-2">
           <div>Players currently unassigned:</div>
           <div className="flex flex-col gap-2">
@@ -56,7 +72,7 @@ const Lobby = () => {
           ))}
         </div>
       </div>
-      <Background className="col-span-2 row-span-2">
+      <Background className="col-span-3 row-span-2">
         <div className="flex h-full flex-col items-center justify-between p-4">
           <div className="flex w-full flex-col items-center gap-3">
             <h1 className="text-3xl font-extrabold">This awesome Quiz</h1>
@@ -98,8 +114,68 @@ const Lobby = () => {
           </div>
         </div>
       </Background>
-      <Background className="col-span-2 row-span-2">
-        <div className="flex flex-col p-2">Chat</div>
+      <Background className="col-span-1 row-span-2">
+        <div className="flex h-full flex-col p-2">
+          <Tabs.Root className="h-full" defaultValue="tab1">
+            <Tabs.List className="flex pb-2">
+              <Tabs.Trigger
+                className="w-full border-b-purple-600 pb-1 data-[state=active]:border-b"
+                value="tab1"
+              >
+                Global
+              </Tabs.Trigger>
+              <Tabs.Trigger
+                className="w-full border-b-purple-600 pb-1 data-[state=active]:border-b"
+                value="tab2"
+              >
+                Team
+              </Tabs.Trigger>
+            </Tabs.List>
+
+            <Tabs.Content value="tab1">
+              <div className="flex h-full flex-col gap-2">
+                {messages.map((message, i) => (
+                  <>
+                    {message.sender === "system" ? (
+                      <p className="text-center text-sm text-zinc-400">
+                        {message.message}
+                      </p>
+                    ) : (
+                      <p className="text-base text-zinc-200">
+                        <span className="font-bold">{message.sender}: </span>
+                        {message.message}
+                      </p>
+                    )}
+                  </>
+                ))}
+              </div>
+            </Tabs.Content>
+            <Tabs.Content value="tab2">
+              <div className="flex h-full flex-col gap-2">
+                {teamMessages.map((message, i) => (
+                  <>
+                    {message.sender === "system" ? (
+                      <p className="text-center text-sm text-zinc-400">
+                        {message.message}
+                      </p>
+                    ) : (
+                      <p className="text-base text-zinc-200">
+                        <span className="font-bold">{message.sender}: </span>
+                        {message.message}
+                      </p>
+                    )}
+                  </>
+                ))}
+              </div>
+            </Tabs.Content>
+          </Tabs.Root>
+          <div className="flex items-center gap-2">
+            <Input placeholder="Enter Message..." />
+            <button className="flex h-full items-center gap-2 rounded-md border p-2 hover:bg-zinc-800">
+              Send <PaperAirplaneIcon className="h-5 w-5" />
+            </button>
+          </div>
+        </div>
       </Background>
     </div>
   );

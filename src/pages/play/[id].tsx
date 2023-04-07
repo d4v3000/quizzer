c9f0239b4a83d0da428ca20ba1269bac6d8facb1
David Glogaza <david.glogaza40@googlemail.com>
2023-04-07 14:56:22 +0200

    add check if lobby exists

---
+++ src/pages/play/[id].tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { LoadingSpinner } from "@ui/Loader";
+
+function Play() {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const [doesLobbyExist, setDoesLobbyExist] = useState(false);
+
+  useEffect(() => {
+    const URL = "http://localhost:3000" || process.env.NODE_URL;
+    // check if lobby exists
+    if (router.query) {
+      axios
+        .post(`${URL}/lobby`, router.query)
+        .then(() => {
+          setIsLoading(false);
+          setDoesLobbyExist(true);
+        })
+        .catch(() => {
+          setIsLoading(false);
+        });
+    }
+  }, [router.query]);
+
+  return (
+    <div className="mx-auto flex h-screen items-center justify-center text-white">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : doesLobbyExist ? (
+        <p className="text-2xl">Lobby exists</p>
+      ) : (
+        <p className="text-2xl">Lobby doesn't exist</p>
+      )}
+    </div>
+  );
+}
+
+export default Play;

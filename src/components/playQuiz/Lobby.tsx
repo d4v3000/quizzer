9a35a483cbabdab6c815fbecbbeb5da1d862fb70
David Glogaza <david.glogaza40@googlemail.com>
2023-04-17 14:31:10 +0200

    move quiz informations to component

--- src/components/playQuiz/Lobby.tsx
+++ src/components/playQuiz/Lobby.tsx
@@ -1,26 +1,14 @@
-import {
-  EyeIcon,
-  EyeSlashIcon,
-  PaperClipIcon,
-} from "@heroicons/react/24/outline";
 import Background from "@ui/Background";
-import Button from "@ui/Button";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
 import * as Tabs from "@radix-ui/react-tabs";
 import { useGameStore } from "@utils/zustand/gameStore";
 import { socket } from "@utils/websocket/socket";
-import Label from "@ui/Label";
-import { SubmitHandler, useForm } from "react-hook-form";
 import Chat from "./Chat";
 import TeamCard from "./TeamCard";
 import NavBar from "./NavBar";
 import JoinForm from "./JoinForm";
-
-interface IFormInputs {
-  userName: string;
-}
+import InformationCard from "./InformationCard";
 
 export interface IPlayer {
   id: string;
@@ -49,9 +37,7 @@ export interface IMessage {
 }
 
 const Lobby = () => {
-  const baseUrl = "http://localhost:3000" || process.env.BASE_URL;
   const router = useRouter();
-  const [showInviteLink, setShowInviteLink] = useState(false);
   const [lobby, setLobby] = useState<ILobby>();
   const [user, setUser] = useState<{
     id?: string;
@@ -187,56 +173,12 @@ const Lobby = () => {
             />
             <div className="grid h-full w-full grid-flow-col grid-cols-6 grid-rows-3 gap-4 overflow-clip">
               <Background className="col-span-2 col-start-3 row-span-2">
-                <div className="flex h-full flex-col items-center justify-between p-4">
-                  <div className="flex w-full flex-col items-center gap-3">
-                    <h1 className="text-3xl font-extrabold">
-                      {lobby?.quizName}
-                    </h1>
-                    <h2 className="text-xl font-bold">
-                      presentend to you by: {lobby?.quizMaster.name}
-                      {lobby?.quizMaster.id === socket.id ? " (You)" : ""}
-                    </h2>
-                    <h3>
-                      You can expect {lobby?.numOfQuestions} Questions this
-                      evening
-                    </h3>
-                  </div>
-                  {isQuizMaster && <Button size="large">Start Game</Button>}
-                  <div className="flex w-1/2 cursor-pointer items-center rounded-2xl border border-zinc-200">
-                    <div
-                      className={`${
-                        showInviteLink ? "cursor-text" : "cursor-pointer"
-                      } flex h-full w-full items-center justify-center gap-2 rounded-2xl rounded-r-none border-0 border-r-2 p-2`}
-                      onClick={() => {
-                        if (!showInviteLink) {
-                          navigator.clipboard.writeText(
-                            baseUrl + router.asPath
-                          );
-                          toast.success("Copied to clipboard!");
-                        }
-                      }}
-                    >
-                      {showInviteLink ? (
-                        <>{router.query.id}</>
-                      ) : (
-                        <>
-                          Copy Invitation Link{" "}
-                          <PaperClipIcon className="h-5 w-5" />
-                        </>
-                      )}
-                    </div>
-                    <div
-                      className="flex items-center justify-center rounded-2xl rounded-l-none p-2"
-                      onClick={() => setShowInviteLink(!showInviteLink)}
-                    >
-                      {showInviteLink ? (
-                        <EyeSlashIcon className="h-6 w-6" />
-                      ) : (
-                        <EyeIcon className="h-6 w-6" />
-                      )}
-                    </div>
-                  </div>
-                </div>
+                <InformationCard
+                  isQuizMaster={isQuizMaster}
+                  numOfQuestions={lobby?.numOfQuestions}
+                  quizMaster={lobby?.quizMaster}
+                  quizName={lobby?.quizName}
+                />
               </Background>
               <Background className="col-span-2 col-start-5 row-span-1 row-start-2">
                 <Tabs.Root

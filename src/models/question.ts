55a4bcdac701a24e78655e1ee3aae19c4ebe111e
David Glogaza <david.glogaza40@googlemail.com>
2023-04-27 19:12:29 +0200

    update types

--- src/models/question.ts
+++ src/models/question.ts
@@ -2,7 +2,22 @@ export interface IQuestion {
   title: string;
   type: "question" | "location" | "guessing";
   imgUrl?: string;
-  answers: IAnswer[];
+  multipleChoiceAnswers: IMultipleChoiceQuestionAnswer | null;
+  guessingAnswer: IGuessingAnswer | null;
+  locationAnswer: ILocationAnswer | null;
+}
+
+export interface IMultipleChoiceQuestionAnswer {
+  answers: { name: string; isCorrect: boolean }[];
+}
+
+export interface IGuessingAnswer {
+  answer: number;
+}
+
+export interface ILocationAnswer {
+  x: number;
+  y: number;
 }
 
 export interface IAnswer {

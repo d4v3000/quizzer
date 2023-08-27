d3ce77759a68b8c1357a85c7883c81ec868f0077
David Glogaza <david.glogaza40@googlemail.com>
2023-08-27 13:51:07 +0200

    add missing width

--- src/components/editQuiz/MainContainer.tsx
+++ src/components/editQuiz/MainContainer.tsx
@@ -6,7 +6,7 @@ const MainContainer = () => {
   const currentQuestion = useQuizStore((state) => state.currentQuestion);
 
   return (
-    <div className="lg:5/6 mx-auto flex w-full flex-col items-center justify-between px-4 lg:px-6 xl:w-3/4 xl:px-10">
+    <div className="mx-auto flex w-full flex-col items-center justify-between px-4 lg:w-5/6 lg:px-6 xl:w-3/4 xl:px-10">
       {currentQuestion === -1 ? <Settings /> : <EditQuestion />}
     </div>
   );
